95eaf31f401e34ffb934b0acaf36fc798c6da26e
ross96D <abelaross96@gmail.com>
2024-09-30 22:54:14 -0400

    update proto models

---
+++ src/proto/root.zig
@@ -0,0 +1,4 @@
+pub const Request = @import("testproto/v1.pb.zig").Request;
+pub const Response = @import("testproto/v1.pb.zig").Response;
+pub const EncryptedPassword = @import("testproto/v1.pb.zig").EncryptedPassword;
+pub const METHOD = @import("testproto/v1.pb.zig").METHOD;
--- src/proto/testproto/v1.pb.zig
+++ src/proto/testproto/v1.pb.zig
@@ -15,18 +15,62 @@ pub const METHOD = enum(i32) {
     METHOD_PUT = 3,
     METHOD_DELETE = 4,
     METHOD_OPTION = 5,
+    METHOD_CONNECT = 6,
+    METHOD_TRACE = 7,
+    METHOD_PATCH = 8,
+    METHOD_HEAD = 9,
     _,
 };
 
 pub const Request = struct {
     method: METHOD = @enumFromInt(0),
+    headers: ArrayList(HeadersEntry),
     url: ManagedString = .Empty,
     body: ManagedString = .Empty,
 
     pub const _desc_table = .{
         .method = fd(1, .{ .Varint = .Simple }),
-        .url = fd(2, .String),
+        .headers = fd(2, .{ .List = .{ .SubMessage = {} } }),
+        .url = fd(3, .String),
+        .body = fd(4, .Bytes),
+    };
+
+    pub const HeadersEntry = struct {
+        key: ManagedString = .Empty,
+        value: ManagedString = .Empty,
+
+        pub const _desc_table = .{
+            .key = fd(1, .String),
+            .value = fd(2, .String),
+        };
+
+        pub usingnamespace protobuf.MessageMixins(@This());
+    };
+
+    pub usingnamespace protobuf.MessageMixins(@This());
+};
+
+pub const Response = struct {
+    status: u32 = 0,
+    headers: ArrayList(HeadersEntry),
+    body: ManagedString = .Empty,
+
+    pub const _desc_table = .{
+        .status = fd(1, .{ .Varint = .Simple }),
+        .headers = fd(2, .{ .List = .{ .SubMessage = {} } }),
         .body = fd(3, .Bytes),
+    };
+
+    pub const HeadersEntry = struct {
+        key: ManagedString = .Empty,
+        value: ManagedString = .Empty,
+
+        pub const _desc_table = .{
+            .key = fd(1, .String),
+            .value = fd(2, .String),
+        };
+
+        pub usingnamespace protobuf.MessageMixins(@This());
     };
 
     pub usingnamespace protobuf.MessageMixins(@This());
